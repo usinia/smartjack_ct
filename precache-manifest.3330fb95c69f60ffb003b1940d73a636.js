70731090c54df1e80fa9f84d851e29403710d0cd
usinia <usinia0103@gmail.com>
2019-12-17 00:19:45 +0900

    Updates

---
+++ precache-manifest.3330fb95c69f60ffb003b1940d73a636.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0f868c6c4215e14cce9f6b3e7385865",
+    "url": "/smartjack_ct/index.html"
+  },
+  {
+    "revision": "3dc6f75ba80f6298acef",
+    "url": "/smartjack_ct/static/js/2.9de3e4f7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/smartjack_ct/static/js/2.9de3e4f7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c2ef6f9056534be60e02",
+    "url": "/smartjack_ct/static/js/main.ab1dfd34.chunk.js"
+  },
+  {
+    "revision": "1a1d04d277077d7c9733",
+    "url": "/smartjack_ct/static/js/runtime-main.212d0001.js"
+  }
+]);
